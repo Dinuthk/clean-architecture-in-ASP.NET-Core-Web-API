212377be77cb2ccd3a4c3044f5201369fc04bd1c
Dinuth Diruksha <dinuthk69@gmail.com>
2025-05-20 16:22:04 +0530

    Update IEmployeeRepository.cs

--- MyApp.Api/MyApp.Core/Interfaces/IEmployeeRepository.cs
+++ MyApp.Api/MyApp.Core/Interfaces/IEmployeeRepository.cs
@@ -9,6 +9,11 @@ namespace MyApp.Core.Interfaces
 {
     public interface GetEmployees
     {
-        Task<IEnumerable<EmployeeEntity>>> GetEmployees();
+        Task<IEnumerable<EmployeeEntity>> GetEmployees();
+        Task<EmployeeEntity> GetEmployeeByIdAsync(Guid id);
+        Task<EmployeeEntity> AddEmployeesAsync(EmployeeEntity entity);
+        Task<EmployeeEntity> UpdateEmployeesAsync(Guid employeeId, EmployeeEntity entity);
+        Task<bool> DeleteEmployeesAsync(Guid employeeId);
+
     }
 }
